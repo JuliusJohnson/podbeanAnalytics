3498cf59d4be0f06559be184ac72e43b9c771f6e
Julius <J3JJamesJ@gmail.com>
2020-10-22 21:25:44 -0400

    full automation for top downloaded podcast

--- google_sheets_top/main.py
+++ google_sheets_top/main.py
@@ -1,19 +1,10 @@
 import browser, upload_top_podcast
-import os
+import os, time, glob
+from datetime import datetime
 
-def runTopPodcast():
+now = datetime.today().strftime("%Y-%m-%d") 
+
+if __name__=='__main__':
     browser.token_get_data()
-    #upload_top_podcast.uploadData() #handles uploading data to Google Spreadsheets and file cleanup
-
-def run_server():
-    pass
-
-runTopPodcast()
-
-'''
-#File Clean Up
-for file in filtered_files:
-        path_to_file = os.path.join("./google_sheets_top",file)
-        os.remove(path_to_file)
-
-'''
+    upload_top_podcast.uploadData() #handles uploading data to Google Spreadsheets and file cleanup
+    os.remove(f'/home/julius/Documents/programming/python/projects/Podbean_Analytics/google_sheets_top/top_podcast_{now}.csv')
